269d332438083b472bc969a3d7ceb330a573a062
rk <rk@rk>
2018-06-10 22:54:55 +0530

    new brRNCH

--- githubTstr/src/rk/gh/test/MainTest.java
+++ githubTstr/src/rk/gh/test/MainTest.java
@@ -4,7 +4,8 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Its Done...");
+		System.out.println("Its Done.");
+		
 	}
 
 }
