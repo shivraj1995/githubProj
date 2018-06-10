62aa6e1cc5588571abc3023f9a71dcb884da926c
Sunil_Shekade <Sunil_Shekade@Sunil-PC>
2018-06-10 21:17:39 +0530

    second modification d

--- githubTstr/src/rk/gh/test/MainTest.java
+++ githubTstr/src/rk/gh/test/MainTest.java
@@ -3,7 +3,8 @@ package rk.gh.test;
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow");
+		System.out.println("Hello");
+		System.out.println("Its Done...");
 	}
 
 }
